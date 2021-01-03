dc3d36f0d8da02b40c50cce239ccd415918c0b08
Sumanth Chowdary <acforotheruse@gmail.com>
2021-01-03 14:30:09 +0530

    Added meta details generator for paginator

---
+++ rest/utils/meta_details_generator.py
@@ -0,0 +1,6 @@
+def meta_details_generator(curr_page, num_pages, items_count):
+    return {
+        "curr_page": curr_page,
+        "pages_count": num_pages,
+        "items_count": items_count
+    }
--- rest/views/post_views/get_posts_view.py
+++ rest/views/post_views/get_posts_view.py
@@ -2,37 +2,36 @@ from django.http import JsonResponse
 from django.core.paginator import Paginator, Page
 
 # Create your views here.
-from rest_framework.views import APIView,Request
+from rest_framework.views import APIView, Request
 from rest_framework.permissions import IsAuthenticated
 
-from rest.models import Post,User
-from rest.utils import object_to_dictionary
+from rest.models import Post, User
+from rest.utils import object_to_dictionary, meta_details_generator
+
+
 class GetPosts(APIView):
-    permission_classes=(IsAuthenticated,)
-    def get(self,request:Request):
-        user:User=request.user
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request: Request):
+        user: User = request.user
         request_data = request.GET
-        page = request_data.get("page",1)
-        per_page = request_data.get("per_page",10)
-        collection=request_data.get("collection")
-        tag=request_data.get("tag")
-        search_query=request_data.get("search_query","")
-        posts=Post.objects.all().filter(user=user,title__contains=search_query)
+        page = request_data.get("page", 1)
+        per_page = request_data.get("per_page", 10)
+        collection = request_data.get("collection")
+        tag = request_data.get("tag")
+        search_query = request_data.get("search_query", "")
+        posts = Post.objects.all().filter(user=user, title__contains=search_query)
         if collection is not None:
-            posts=posts.filter(collection=collection)
+            posts = posts.filter(collection=collection)
         if tag is not None:
-            posts=posts.filter(tag=tag)
+            posts = posts.filter(tag=tag)
         paginator: Paginator = Paginator(posts, per_page)
         page_obj: Page = paginator.get_page(page)
         query_posts = []
         for post in page_obj.object_list:
-            curr_post=object_to_dictionary(post)
+            curr_post = object_to_dictionary(post)
             query_posts.append(curr_post)
-        meta = {
-            "currPage": page_obj.number,
-            "pages_count": paginator.num_pages,
-            "items_count": paginator.count,
-        }
+        meta = meta_details_generator(
+            page_obj.number, paginator.num_pages, paginator.count)
         response_data = {"posts": query_posts, "meta": meta}
         return JsonResponse(response_data)
-
