af6577c069e85a96a6fa2b4c3185e58f7c1fb9ff
Sumanth Chowdary <acforotheruse@gmail.com>
2021-04-03 08:32:24 +0530

    Added Fetching Individual Project

--- rest/views/project_views/get_projects_view.py
+++ rest/views/project_views/get_projects_view.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView, Request
 from rest_framework.permissions import IsAuthenticated
 from rest.models import User, Project
 from rest.utils import meta_details_generator
+from rest.utils import bad_request
 
 
 class GetProjects(APIView):
@@ -11,9 +12,28 @@ class GetProjects(APIView):
 
     def get(self, request: Request):
         user: User = request.user
+        project_id = request.GET.get("project_id", None)
         page = request.GET.get("page", 1)
         per_page = request.GET.get("per_page", 10)
         project_objects = Project.objects.all().filter(user=user)
+        print(project_id)
+        if (project_id):
+            projectSet = project_objects.filter(id=project_id)
+            if not projectSet:
+                return bad_request("No Such Project Exists!")
+            project = projectSet[0]
+            curr_project = {
+                "id": project.id,
+                "name": project.name,
+                "link": project.link,
+                "description": project.description
+            }
+            if project.image:
+                image_url_raw = project.image.url
+                curr_project["image"] = request.build_absolute_uri(
+                    image_url_raw)
+            response_data = {"project": curr_project}
+            return JsonResponse(response_data)
         paginator = Paginator(project_objects, per_page)
         page_obj: Page = paginator.get_page(page)
         project_list = []
