509a1ef88a2f3513a78d9110a6ae9ba5cd2b814b
Sumanth Chowdary <acforotheruse@gmail.com>
2021-04-28 14:58:56 +0530

    Set Project Name Length To 100

--- rest/models/project_models/project_model.py
+++ rest/models/project_models/project_model.py
@@ -1,9 +1,10 @@
 from django.db import models
 from rest.models import User
 
+
 class Project(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
     description = models.TextField()
     link = models.URLField()
     image = models.ImageField(blank=True)
-    user =  models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
